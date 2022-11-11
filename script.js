a20399ba9ed91e8a2f77ad3f5362aa58ce4721b0
dapoadedire <adedireadedapo19@gmail.com>
2022-11-11 19:57:33 +0100

    fix: found usage of this outside of class or classes-like object

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
 const images = [
     {
         id: 1,
-        src: "https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "image1",
+        src: 'https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        alt : 'image1'
     },
     {
         id: 2,
-        src: "https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "image2",
+        src: 'https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        alt : 'image2'
     },
     {
         id: 3,
-        src: "https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "image3",
+        src: 'https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        alt : 'image3'
     },
     {
         id: 4,
-        src: "https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "image4",
+        src: 'https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        alt : 'image4'
     },
     {
         id: 1,
-        src: "https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "image1",
+        src: 'https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        alt : 'image1'
     },
     {
         id: 2,
-        src: "https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "image2",
+        src: 'https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        alt : 'image2'
     },
     {
         id: 3,
-        src: "https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "image3",
+        src: 'https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        alt : 'image3'
     },
     {
         id: 4,
-        src: "https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "image4",
+        src: 'https://images.unsplash.com/photo-1556740732-21e6bd3ab135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        alt : 'image4'
     },
-];
 
-const container = document.querySelector(".container");
+]
+
+const container = document.querySelector('.container');
+
 
 // Create a div with class name image
 // Create a figure element
@@ -58,14 +60,15 @@ const container = document.querySelector(".container");
 // Return the div element
 
 const createImage = (image) => {
-    const div = document.createElement("div");
-    div.classList.add("image");
+    const div = document.createElement('div');
+    div.classList.add('image');
 
-    const figure = document.createElement("figure");
-    const figcaption = document.createElement("figcaption");
-    const img = document.createElement("img");
+
+    const figure = document.createElement('figure');
+    const figcaption = document.createElement('figcaption');
+    const img = document.createElement('img');
     figcaption.textContent = image.alt;
-
+   
     img.src = image.src;
     img.alt = image.alt;
     figure.appendChild(img);
@@ -73,20 +76,23 @@ const createImage = (image) => {
     div.appendChild(figure);
 
     return div;
-};
+}
+
+
 
 // Loop through the images array
 // Create an image element for each image
 
-images.forEach((image) => {
+images.forEach(image => {
     container.appendChild(createImage(image));
-});
+}
+)
+
 
 //  Check if the image is loaded or not
 //  ANd add another image.
-document.querySelectorAll("img").forEach(
-    (img) =>
-    (img.onerror = () => {
-        this.src = "assets/images/notfound.png";
-    })
-);
+document.querySelectorAll('img').forEach(img => img.onerror = function() {
+    this.src = 'assets/images/notfound.png';
+})
+
+
