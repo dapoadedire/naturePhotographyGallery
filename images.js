c9ce0a323df8c61b3ae34d8a15f7d544c5274a16
dapoadedire <adedireadedapo19@gmail.com>
2022-11-12 16:18:56 +0100

    fix: optimize

--- images.js
+++ images.js
@@ -2,19 +2,19 @@ export const images = [
     // q_auto,f_auto
    
     {
-        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto/v1668264057/nature-photos/img9_nr3elp.jpg",
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264057/nature-photos/img9_nr3elp.jpg",
         alt: "Tall palm trees.",
     },
     {
-        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto/v1668264029/nature-photos/img1_ydjt5t.jpg",
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264029/nature-photos/img1_ydjt5t.jpg",
         alt: "Banana/plantain tree.",
     },
     {
-        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto/v1668264033/nature-photos/img10_e65lod.jpg",
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264033/nature-photos/img10_e65lod.jpg",
         alt: "Field.",
     },
     {
-        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto/v1668264031/nature-photos/img7_bo6wnn.jpg",
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264031/nature-photos/img7_bo6wnn.jpg",
         alt: "Cloudy sky.",
     },
  
