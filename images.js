af1606be214272e85d866d413abe41d289e6dc0b
dapoadedire <adedireadedapo19@gmail.com>
2022-11-13 15:34:56 +0100

    feat: duplicate images

--- images.js
+++ images.js
@@ -1,6 +1,14 @@
 export const images = [
     // q_auto,f_auto
-   
+
+    {
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264057/nature-photos/img9_nr3elp.jpg",
+        alt: "Tall palm trees.",
+    },
+    {
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264029/nature-photos/img1_ydjt5t.jpg",
+        alt: "Banana/plantain tree.",
+    },
     {
         src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264057/nature-photos/img9_nr3elp.jpg",
         alt: "Tall palm trees.",
@@ -17,7 +25,31 @@ export const images = [
         src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264031/nature-photos/img7_bo6wnn.jpg",
         alt: "Cloudy sky.",
     },
- 
-  
+    {
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264029/nature-photos/img1_ydjt5t.jpg",
+        alt: "Banana/plantain tree.",
+    },
+    {
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264057/nature-photos/img9_nr3elp.jpg",
+        alt: "Tall palm trees.",
+    },
+    {
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264033/nature-photos/img10_e65lod.jpg",
+        alt: "Field.",
+    },
+    {
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264031/nature-photos/img7_bo6wnn.jpg",
+        alt: "Cloudy sky.",
+    },
+    {
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264029/nature-photos/img1_ydjt5t.jpg",
+        alt: "Banana/plantain tree.",
+    },
+    {
+        src: "https://res.cloudinary.com/dapoadedire/image/upload/q_auto,f_auto,g_auto/v1668264057/nature-photos/img9_nr3elp.jpg",
+        alt: "Tall palm trees.",
+    },
+
+
 
 ]
